f27a33b9608cf12f6e57846d033e7f10a189e2f1
Carey Git <CareyGit@163.com>
2015-07-17 15:42:59 +0800

    Init Commit

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index()
+        {
+            return "Hi,Carey! \r\n Current Time is: "+ DateTime.Now.ToString("yy-MMM-dd ddd HH:mm:ss");
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product product = new Product()
+            {
+                Category = "Watersports",
+                Desp = "A boat for one person",
+                Id = 100,
+                Name = "Carey",
+                Price = 275M
+            };
+            return View("Result", (object) string.Format("Product name:{0}", product.Name));
+        }
+
+        public ViewResult UseExtension()
+        {
+            // create and populate ShoppingCart
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+
+
+            // get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+            return View("Result",
+                (object) String.Format("Total: {0:c}", cartTotal));
+        }
+
+        //public ViewResult UseFilterExtensionMdthod()
+        //{
+        //    IEnumerable<Product> products = new ShoppingCart
+        //    {
+        //        Products = new List<Product> {
+        //            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+        //            new Product {Name = "Lifejacket", Category = "Watersports",Price = 48.95M},
+        //            new Product {Name = "Soccer ball", Category = "Soccer",Price = 19.50M},
+        //            new Product {Name = "Corner flag", Category = "Soccer",Price = 34.95M}
+        //        }
+        //    };
+        //    var foundProducts = products.OrderByDescending(e => e.Price).Take(3).Select(e => new { e.Name, e.Price });
+
+        //}
+    }
+}
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Desp { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+    }
+
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            return cartParam.Products.Sum(prod => prod.Price);
+        }
+
+        
+    }
+}
